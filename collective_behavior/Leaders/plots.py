8d2d06c3aaa12839df0a94f0947890ef8ff06387
Francesca Rossi <francesca.rossi521@gmail.com>
2026-05-02 14:53:08 -0700

    Add files via upload

--- collective_behavior/Leaders/plots.py
+++ collective_behavior/Leaders/plots.py
@@ -24,13 +24,12 @@ plt.rcParams.update({
 })
 
 dt = 0.05
-max_steps = 1220
-informed_boids = 4
+max_steps = 440
+informed_boids = 0
 
 polarization_history = np.load(f"polarization_history.npy")
 #milling_history = np.load(f"milling_history.npy")
 weights_history = np.load(f"weights_history.npy")
-distance_history = np.load(f"distance_history.npy")
 
 cmap = plt.get_cmap('viridis')
 
@@ -38,7 +37,7 @@ cmap = plt.get_cmap('viridis')
 def plot_weights_simplex(weights_history, agent_indices, dt=0.05, save_path="weights_simplex.pdf"):
     scale = 1.0
     n_agents = len(agent_indices)
-    fig, axs = plt.subplots(n_agents, 1, figsize=(5, 5 if n_agents == 1 else 5*n_agents+1))  # Adjust height dynamically
+    fig, axs = plt.subplots(n_agents, 1, figsize=(5, 5 if n_agents == 1 else 10))  # Adjust height dynamically
     fig.patch.set_facecolor('white')
 
     # Ensure axs is iterable even for 1 subplot
@@ -81,12 +80,22 @@ def plot_weights_simplex(weights_history, agent_indices, dt=0.05, save_path="wei
 
         tax.ticks(axis='lbr', multiple=0.2, offset=0.035, linewidth=1, fontsize=FS, tick_formats="%.1f")
         tax.clear_matplotlib_ticks()
+        tax._redraw_labels()
+
+    '''# --- Single shared colorbar (to the right, not overlapping) ---
+    cbar_ax = fig.add_axes([0.80, 0.25, 0.02, 0.5])  # [left, bottom, width, height]
+    cbar = fig.colorbar(sm, cax=cbar_ax, orientation='vertical')
+
+    tick_locs = np.linspace(0, len(points) - 1, 2)
+    cbar.set_ticks(tick_locs)
+    cbar.set_ticklabels([f"{i * dt:.0f}" for i in tick_locs], fontsize=FS)
+    cbar.set_label("$t$ [s]", fontsize=FS)'''
 
     plt.savefig(save_path, dpi=300, bbox_inches='tight')
     print(f"Figure saved as {save_path}")
 
 
-tracked_index = [0, 39]     # one informed and one not
+tracked_index = [0]
 plot_weights_simplex(weights_history[:max_steps], agent_indices=tracked_index)
 
 
@@ -103,6 +112,7 @@ for idx, traj in enumerate(trajectories):
     if len(traj) >= 2:
         traj = np.array(traj)
         segments = np.array([[traj[i], traj[i + 1]] for i in range(len(traj) - 1)])
+        #colors = plt.cm.viridis(np.linspace(0, 1, len(segments)))
         colors = cmap(np.linspace(0, 1, len(segments)))
         lc = LineCollection(segments, colors=colors, linewidths=1.5)
         ax.add_collection(lc)
@@ -115,50 +125,40 @@ for idx, traj in enumerate(trajectories):
 
     ax.scatter(traj[-1,0], traj[-1,1], color=color, edgecolors='black', s=80, zorder=3)
 
-goal_point = np.array([-15, -15])
-goal_scat = ax.scatter(goal_point[0], goal_point[1], edgecolors='black', linewidths=1.3, facecolor='gold',
-                       marker='*', s=400, zorder=2, label='Goal')
 
 ax.set_aspect('equal')
 ax.set_xticks([])
 ax.set_yticks([])
-ax.set_xlim([-26,10])
-ax.set_ylim([-26,10])
+ax.set_xlim([-22,5])
+ax.set_ylim([-5,22])
 plt.tight_layout()
 plt.savefig("trajectories.pdf", dpi=300, bbox_inches='tight')
 
-
 # -------------------------------
-# Plot metrics
-fig, axs = plt.subplots(2, 1, figsize=(9, 10), sharex=True)
+# Polarization plot
 # Time array
 time = np.arange(len(polarization_history[:max_steps])) * dt
-# --- Polarization plot ---
-axs[0].plot(time, polarization_history[:max_steps], linewidth=2)
-axs[0].set_ylabel(f"$p$", fontsize=FS+4)
-axs[0].set_ylim(0, 1.05)
+fig, ax = plt.subplots(figsize=(9, 5))
+ax.plot(time, polarization_history[:max_steps], linewidth=2)
+ax.set_ylabel(f"$p$", fontsize=FS+4)
+ax.set_xlabel(f"Time [s]", fontsize=FS+4)
+ax.set_ylim(0, 1.05)
 # set tick positions every 0.2
 ax.set_yticks(np.arange(0, 1.01, 0.2))
 # format tick labels to 1 decimal place
 ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.1f'))
-# --- Distance to goal plot ---
-axs[1].plot(time, distance_history[:max_steps], linewidth=2)
-axs[1].set_ylabel("$d$", fontsize=FS+4)
-axs[1].set_xlabel(f"Time [s]", fontsize=FS+4)
-# Styling
-for ax in axs:
-    ax.set_xlim(0, time[-1])
-    ax.tick_params(axis='both', labelsize=FS+4)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
+ax.set_xlim(0, time[-1])
+ax.tick_params(axis='both', labelsize=FS+4)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
 plt.tight_layout()
-plt.savefig("polarization_distance.pdf", dpi=300)
+plt.savefig("polarization.pdf", dpi=300)
 plt.show()
 
 
 # -------------------------------
 # Video parameters
-fps = int(2 / dt)   # double speed
+fps = int(2 / dt)   # same speed as before
 max_traj_length = 40
 
 # Load trajectories
@@ -166,11 +166,12 @@ with open("boids_trajectories.pkl", "rb") as f:
     all_trajectories = pickle.load(f)
 
 num_boids = len(all_trajectories)
+#max_steps = max(len(traj) for traj in all_trajectories)
 
 # Figure setup
 fig, ax = plt.subplots(figsize=(8, 8))
-ax.set_xlim([-26,10])
-ax.set_ylim([-26,10])
+ax.set_xlim([-22, 5])
+ax.set_ylim([-5, 22])
 ax.set_xticks([])
 ax.set_yticks([])
 ax.set_aspect("equal")
@@ -182,18 +183,14 @@ for _ in range(num_boids):
     ax.add_collection(line)
     trajectory_collections.append(line)
 
-goal_scat = ax.scatter(goal_point[0], goal_point[1], edgecolors='black', linewidths=1.3, facecolor='gold',
-                       marker='*', s=400, zorder=1)
-
 # Boid scatter
-colors = ["red"] * informed_boids + ["blue"] * (num_boids-informed_boids)  # adjust if needed
+colors = ["blue"] * num_boids  # adjust if needed
 scat = ax.scatter(
     [all_trajectories[i][0][0] for i in range(num_boids)],
     [all_trajectories[i][0][1] for i in range(num_boids)],
     edgecolors="black",
     s=50,
-    c=colors,
-    zorder=2
+    c=colors
 )
 
 # Writer
@@ -232,6 +229,8 @@ print(f"Video saved as '{video_name}'")
 
 # -------------------------------
 # ALL-BOIDS SIMPLEX GRID PLOT
+
+num_boids = len(weights_history[0])
 cols = 5
 rows = int(np.ceil(num_boids / cols))
 
@@ -244,7 +243,8 @@ fig = plt.figure(figsize=(a4_width_in, fig_height_in))
 fig.patch.set_facecolor('white')
 
 spec = gridspec.GridSpec(rows, cols, wspace=0.5, hspace=0.0)
-norm = mpl.colors.Normalize(vmin=0, vmax=len(weights_history[:max_steps]) - 1)
+
+norm = mpl.colors.Normalize(vmin=0, vmax=len(weights_history) - 1)
 
 
 def plot_simplex_subplot(ax, weights_history, agent_index, cmap, norm, show_labels=False):
@@ -270,9 +270,9 @@ def plot_simplex_subplot(ax, weights_history, agent_index, cmap, norm, show_labe
     )
 
     if show_labels:
-        tax.left_axis_label("$w^1$ (separation)", fontsize=9, offset=0.38)
-        tax.right_axis_label("$w^2$ (alignment)", fontsize=9, offset=0.38)
-        tax.bottom_axis_label("$w^3$ (cohesion)", fontsize=9, offset=0.65)
+        tax.left_axis_label("separation", fontsize=9, offset=0.38)
+        tax.right_axis_label("alignment", fontsize=9, offset=0.38)
+        tax.bottom_axis_label("    cohesion", fontsize=9, offset=0.65)
         tax.ticks(axis='lbr', multiple=0.5, offset=0.065, fontsize=8, linewidth=0.5, tick_formats="%.1f")
 
     tax._redraw_labels()
